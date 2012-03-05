ffa78c8dd83ddecc6073a179698ea1228dea8aeb
André Panisson <panisson@gmail.com>
2012-03-05 19:39:32 +0100

    Add /stream endpoint to stream graph.json data

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import SocketServer
+import time
 
 class RequestHandler(SimpleHTTPRequestHandler):
 
@@ -10,7 +11,21 @@ class RequestHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
 
-        SimpleHTTPRequestHandler.do_GET(self)
+        if self.path == "/stream":
+            
+            self.send_response(200)
+            self.send_header("Content-Type", "application/x-javascript")
+            self.send_header("Tranfer-Encoding", "chunked")
+            self.end_headers()
+            f = open("graph.json", 'r');
+            line = f.readline()
+            while line != '':
+                self.wfile.write(line)
+                time.sleep(0.1)
+                line = f.readline()
+
+        else:
+            SimpleHTTPRequestHandler.do_GET(self)
 
 
 PORT = 8080
